1265627f5bfa80183d675100b7a45bc746ac6c0a
Paul Crowley <github@paul.ciphergoth.org>
2025-08-29 22:07:45 -0700

    pin to a tarjan revision

--- tarjan-cli.nix
+++ tarjan-cli.nix
@@ -1,13 +1,21 @@
 { pkgs ? import <nixpkgs> {}, tarjanSrc ? /Users/paul/g/zerbongle/tarjan }:
 
+let
+  # Pin to specific git commit for reproducibility
+  pinnedSrc = builtins.fetchGit {
+    url = tarjanSrc;
+    rev = "f27f8bf4213467746877e44f1ed15fba720e2bff";
+  };
+in
+
 pkgs.rustPlatform.buildRustPackage {
   pname = "tarjan-cli";
-  version = "0.1.0";
+  version = "0.1.0-f27f8bf";
   
-  src = tarjanSrc;
+  src = pinnedSrc;
   
   cargoLock = {
-    lockFile = tarjanSrc + "/Cargo.lock";
+    lockFile = pinnedSrc + "/Cargo.lock";
   };
   
   meta = {
